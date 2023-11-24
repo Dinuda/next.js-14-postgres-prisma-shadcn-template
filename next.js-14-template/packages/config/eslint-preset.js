cddcccdcf8da940cdcf7f52b348c8d7fddaf43b9
Turbobot <turbobot@vercel.com>
2023-11-24 17:01:16 +0530

    feat(create-turbo): apply official-starter transform

---
+++ next.js-14-template/packages/config/eslint-preset.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ["next", "turbo", "prettier"],
+  settings: {
+    next: {
+      rootDir: ["apps/*/", "packages/*/"],
+    },
+  },
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+  },
+  parserOptions: {
+    babelOptions: {
+      presets: [require.resolve("next/babel")],
+    },
+  },
+};
